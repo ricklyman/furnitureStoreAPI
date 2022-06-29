0de61d69d80c9433aac90f46c86868b8ac1d5b60
Rick Lyman <lyman.rick@gmail.com>
2022-06-29 18:28:46 -0400

    Add files via upload

---
+++ src/test/java/com/furniture_store_api/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furniture_store_api.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
